49f695494368655ae596d4baa1a9bb253d92818e
tanmay2006 <tanmayislam101@gmail.com>
2026-01-26 00:07:50 +0600

    feat: add user, product, and sales modules with controllers and services

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ProductModule, SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
