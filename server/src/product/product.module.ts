41eff896688fffa47212d3233bb71d1ef111aa9b
tanmay2006 <tanmayislam101@gmail.com>
2026-01-26 02:03:19 +0600

    feat(auth): integrate AuthModule and update guards in product module and controller

--- server/src/product/product.module.ts
+++ server/src/product/product.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
-import { AuthModule } from 'src/common/auth.module';
+import { AuthModule } from '../common/auth.module';
 
 @Module({
   imports: [AuthModule],
